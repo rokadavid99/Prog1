c296e069d5f93a07cf2b808d06a5beeaeb2776b0
Róka Dávid <43438252+rokadavid99@users.noreply.github.com>
2019-03-19 11:51:03 +0100

    Update labda.c

--- labda.c
+++ labda.c
@@ -1,60 +1,39 @@
-#include <stdio.h>
-#include <curses.h>
-#include <unistd.h>
+#include <curses.h> 
+#include <unistd.h> 
+#include <stdlib.h> 
 
-
-main ( void )
+int main ( void )
 {
-    WINDOW *ablak;
-    ablak = initscr ();
 
-    int x = 0;
-    int y = 0; 
+    WINDOW *ablak;   	
+    ablak = initscr (); 
+    int mx;  	
+    int my;
+
+    int xj = 0, xk = 0, yj = 0, yk = 0; /
+
+    for ( ;; ) {	
+
+
+        getmaxyx ( ablak, my , mx ); 			    
    
-    int deltax = 1; 
-    int deltay = 1; 
+	      xj = (xj - 1) % mx;	
+        xk = (xk + 1) % mx;
 
-    int mx; 
-    int my; 
-    getmaxyx ( ablak, my , mx ); 
-    int magassag[my];
-    int szelesseg[mx];
-    
+        yj = (yj - 1) % my;
+        yk = (yk + 1) % my;						
+				     		
+	    
+        mvprintw ( abs (yj + (my - yk) ), abs (xj + (mx - xk)) , "O" ); 
 
-    //Feltételes utasítások helyett most for ciklusokat használunk
+        refresh ();		
+        usleep ( 50000);	
 
-
-    for(int i=0;i<mx;i++){
-        szelesseg[i]=1;
-    }
-    
-     for(int i=0;i<my;i++){
-    magassag[i]=1;
-    }
-    
-
-    //A keret elérése miatt a kezdő értéket és az utolsó előtti értéket megváltoztatjuk a többi elem abszolút értékére
+	clear();		
 
 
-    szelesseg[0]=-1;
-    magassag[0]=-1;
-    szelesseg[mx-1]=-1;
-    magassag[my-1]=-1;
-    
-    for ( ;; ) {
 
-        
- 
-        mvprintw ( y, x, "0" );
 
-        refresh ();
-        usleep ( 100000 );
-
-        x = x + deltax; 
-        y = y + deltay; 
-
-        deltax*=szelesseg[x];
-        deltay*=magassag[y];
     }
 
     return 0;
